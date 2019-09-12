98e93a1eac25084dbf154510b7f3c391be66fcc7
Pedro Salazar Paredes <pdrslzr@gmail.com>
2019-09-11 19:44:27 -0500

    typo fix
    ^

--- express-backend/env.sh
+++ express-backend/env.sh
@@ -1,4 +1,4 @@
-echo GRAPHENEDB_BOLT_PASSWORD='b.ht1Xh7M1OWjL.X6EjWbzAOGkdgjvY'
-echo GRAPHENEDB_BOLT_URL='bolt://hobby-ofgfbcogogopgbkeeldjondl.dbs.graphenedb.com:24787'
-echo GRAPHENEDB_BOLT_USER='app145874852-6AHnXE'
-echo MONGODB_URI='mongodb://heroku_6tqp5v23:aum0s1qvsk2u4kpp6lk5agbrcs@ds235788.mlab.com:35788/heroku_6tqp5v23'
+export GRAPHENEDB_BOLT_PASSWORD='b.ht1Xh7M1OWjL.X6EjWbzAOGkdgjvY'
+export GRAPHENEDB_BOLT_URL='bolt://hobby-ofgfbcogogopgbkeeldjondl.dbs.graphenedb.com:24787'
+export GRAPHENEDB_BOLT_USER='app145874852-6AHnXE'
+export MONGODB_URI='mongodb://heroku_6tqp5v23:aum0s1qvsk2u4kpp6lk5agbrcs@ds235788.mlab.com:35788/heroku_6tqp5v23'
