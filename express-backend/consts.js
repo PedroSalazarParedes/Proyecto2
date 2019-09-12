8b021b69eef5daafe1de625933761baafad0ea2c
David Narvaez <dnarvaez27@outlook.com>
2019-09-12 15:45:56 -0500

    Fix in Neo4j query resolver

--- express-backend/consts.js
+++ express-backend/consts.js
@@ -2,35 +2,47 @@
 "use strict";
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
-const neo4j = require("neo4j-driver").v1;
+const neo4j = require('neo4j-driver').v1;
+
 
 const url = process.env.MOGODB_URI; // Connection URL
 const dbName = "hungryffDB"; // Database Name
-const driver = neo4j.driver(process.env.GRAPHENEDB_BOLT_URL, neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD));
+const driver = neo4j.driver(uri, neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD))
 
 
+async const execGraphQuery = (query, params) => {
+  return new Promise((resolve, reject) => {
+    driver.session()
+      .run(query, params)
+      .subscribe({
+        onNext: resolve,
+        onCompleted: () => session.close(),
+        onError: reject
+      });
+  });
+};
 
-
-const execQuery = function ( collection_name, query, callback, limit ) {
-  const createQuery = function ( db, collection_name, query = {}, callback, limit = 100 ) {
-    db.collection( collection_name )
-      .find( query )
-      .limit( limit )
-      .toArray( ( err, docs ) => {
-        assert.equal( err, null );
-        console.log( `Found ${docs.length} records` );
-        callback( docs );
-      } );
+const execQuery = function (collection_name, query, callback, limit) {
+  const createQuery = function (db, collection_name, query = {}, callback, limit = 100) {
+    db.collection(collection_name)
+      .find(query)
+      .limit(limit)
+      .toArray((err, docs) => {
+        assert.equal(err, null);
+        console.log(`Found ${docs.length} records`);
+        callback(docs);
+      });
   };
 
-  MongoClient.connect( url, ( err, client ) => {
-    assert.equal( null, err );
-    console.log( "Connected successfully to server" );
-    createQuery( client.db( dbName ), collection_name, query, callback, limit );
+  MongoClient.connect(url, (err, client) => {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    createQuery(client.db(dbName), collection_name, query, callback, limit);
     client.close();
-  } );
+  });
 };
 
 module.exports = {
-  execQuery
+  execQuery,
+  execGraphQuery
 };
