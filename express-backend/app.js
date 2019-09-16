f1efe3b83183baddcd8646c757744389706b6c25
David Narvaez <dnarvaez27@outlook.com>
2019-09-16 10:48:04 -0500

    Front

--- express-backend/app.js
+++ express-backend/app.js
@@ -1,44 +1,49 @@
 /* eslint-disable no-undef,no-unused-vars */
 "use strict";
-const createError = require( "http-errors" );
-const express = require( "express" );
-const cors = require( "cors" );
-const path = require( "path" );
-const cookieParser = require( "cookie-parser" );
-const logger = require( "morgan" );
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require( "./routes/index" );
-const usersRouter = require( "./routes/users" );
-const recipesRouter = require( "./routes/recipe" );
-const ingredientsRouter = require( "./routes/ingredient" );
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const recipesRouter = require("./routes/recipe");
+const ingredientsRouter = require("./routes/ingredient");
 
 const app = express();
 
-app.use( logger( "dev" ) );
-app.use( express.json() );
-app.use( express.urlencoded( { extended: false } ) );
-app.use( cookieParser() );
-app.use( express.static( path.join( __dirname, "front/build" ) ) );
-app.use( cors( { origin: "http://localhost:3000" } ) );
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "front")));
+app.use(cors({ origin: ["http://localhost:3000", "https://hangryff.herokuapp.com"] }));
 
 // Routes
-app.use( "/api", indexRouter );
-app.use( "/api/users", usersRouter );
-app.use( "/api/recipes", recipesRouter );
-app.use( "/api/ingredients", ingredientsRouter );
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/recipes", recipesRouter);
+app.use("/api/ingredients", ingredientsRouter);
 
 // catch 404 and forward to error handler
-app.use( ( req, res, next ) => next( createError( 404 ) ) );
+app.use((req, res, next) => next(createError(404)));
 
 // error handler
-app.use( ( err, req, res, next ) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get( "env" ) === "development" ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status( err.status || 500 );
-  res.send( "error" );
-} );
+  res.status(err.status || 500);
+  res.send("error");
+});
+
+// app.get('/', (req, res) => {
+//   console.log('holi');
+//   res.sendFile('./front/index.html')
+// })
 
 module.exports = app;
---
+++ express-backend/front/precache-manifest.c1ec688417317e7ea03fb4f73dfb0de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0056d7d1d07ad5a6316d4619ef66e1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c50cce1be7f9c4fcd32e",
+    "url": "/static/css/main.f930b177.chunk.css"
+  },
+  {
+    "revision": "13c8d760ff8fcb9f1238",
+    "url": "/static/js/2.0a7b9ecb.chunk.js"
+  },
+  {
+    "revision": "c50cce1be7f9c4fcd32e",
+    "url": "/static/js/main.1af740f9.chunk.js"
+  },
+  {
+    "revision": "d0e05eca174d266a970f",
+    "url": "/static/js/runtime~main.3df769b5.js"
+  }
+]);
