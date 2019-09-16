a34fe5d123ecae6b088e180c021d7353b421e2e6
Pedro Salazar Paredes <pdrslzr@gmail.com>
2019-09-16 10:39:08 -0500

    Merge branch 'master' of https://github.com/PedroSalazarParedes/Proyecto2
    fv

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -4,6 +4,13 @@ const express = require("express");
 const router = express.Router();
 const { execQuery } = require( "../consts" );
 
+/* POST user auth */
+
+router.post("/auth", (req, res) => {
+  execQuery("users", {"username" : {$eq: req.body.username}}, data => res.send(data));
+});
+
+
 /* GET users listing. */
 router.get("/",(req, res, next) => {
   execQuery( "users", {}, ( users ) => res.send( users ) );
